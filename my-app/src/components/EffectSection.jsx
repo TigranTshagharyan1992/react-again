858af9cfb70c74e1f47a2c84f226baa959ca2f3b
Tigran <tshagharyant20@gmail.com>
2023-12-29 18:47:24 +0400

    use effect learn

--- my-app/src/components/EffectSection.jsx
+++ my-app/src/components/EffectSection.jsx
@@ -1,14 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from "./ui/Button";
 import Modal from "./Modal/Modal";
 
 const EffectSection = () => {
 
     const [showModal, setShowModal] = useState(false);
+    const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(false);
 
     function openModal() {
         setShowModal(!showModal);
     }
+
+    async function getUsers() {
+        setLoading(true);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json();
+        setUsers(users);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getUsers();
+    },[])
+
+
 
     return (
         <section>
@@ -22,6 +38,14 @@ const EffectSection = () => {
                     molestiae rerum saepe suscipit! Aspernatur?</p>
                <Button onClick={()=>setShowModal(false)}>Close modal</Button>
             </Modal>
+            {loading && <p>Loading...</p>}
+            {!loading &&  <ul>
+                {users.map((user,index) => {
+                    return <li key={index}>{user.name}</li>
+                    })
+                }
+
+            </ul>}
         </section>
     );
 };

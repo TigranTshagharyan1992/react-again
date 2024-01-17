0ed6870915fcd1a695f045510b4819e5c170598c
Tigran <tshagharyant20@gmail.com>
2024-01-17 18:37:40 +0400

    finish this video lessons

--- my-app/src/components/EffectSection.jsx
+++ my-app/src/components/EffectSection.jsx
@@ -2,9 +2,10 @@ import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import Button from "./ui/Button";
 import Modal from "./Modal/Modal";
 import {generateKey} from "../helper";
+import useInput from "../hooks/useInput";
 
 const EffectSection = () => {
-
+    const input = useInput();
     const [showModal, setShowModal] = useState(false);
     const [users,setUsers] = useState([]);
     const [loading,setLoading] = useState(false);
@@ -12,19 +13,13 @@ const EffectSection = () => {
     function openModal() {
         setShowModal(!showModal);
     }
-    const getUsers = async ()=>{
+    const getUsers = useCallback(async ()=>{
         setLoading(true);
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const users = await response.json();
         setUsers(users);
         setLoading(false);
-    }
-   useMemo(()=>{
-        getUsers();
-    },[])
-
-
-
+    },[]);
 
     useEffect(() => {
         getUsers();
@@ -44,13 +39,19 @@ const EffectSection = () => {
                <Button onClick={()=>setShowModal(false)}>Close modal</Button>
             </Modal>
             {loading && <p>Loading...</p>}
-            {!loading &&  <ul>
-                {users.map((user,index) => {
-                    return <li key={generateKey(index)}>{user.name}</li>
-                    })
-                }
 
-            </ul>}
+            {!loading &&
+                <>
+                    <input type="text" {...input} />
+                    <ul>
+                        {users.map((user,index) => {
+                            return <li key={generateKey(index)}>{user.name}</li>
+                        })
+                        }
+
+                    </ul>
+                </>
+            }
         </section>
     );
 };
